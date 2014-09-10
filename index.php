0d551a73ee36d6dab863db9a03f4d48c22565447
Mike Street <mikestreety@gmail.com>
2014-09-10 06:26:28 -0600

    New theme for the site

--- index.php
+++ index.php
@@ -57,59 +57,62 @@
 		</script>
 	</head>
 	<body>
-		<article class="wrap hero">
-	  		<h1>Hovélo</h1>
-			<p class="meta">We're a friendly group of riders with one dedicated goal - to have&nbsp;fun. If you'd like to ride with us, we'd love to see you at the weekend.</p>
-			<p><strong>7:30am</strong> every <strong>Saturday</strong> and <strong>8:00am</strong> every <strong>Sunday</strong> outside the Goldstone Small Batch Coffee Company</p>
-			<p class="social-wrap">
-				<a href="http://www.twitter.com/_hovelo" rel="nofollow"  class="social twitter">@_hovelo</a>
-				<a href="http://www.facebook.com/hovelo" rel="nofollow"  class="social facebook">Hovélo on facebook</a>
-				<a href="http://www.strava.com/clubs/hovelo" rel="nofollow" class="social strava">Hovélo on Strava</a>
-			</p>
+		<div class="wrap">
+			<a class="link-top" href="http://members.hovelo.co.uk">Members Area</a>
+		</div>
+		<article class="wrap plain layout-two">
+			<div class="column"><h1>Hovélo</h1></div>
+			<div class="column">
+				<p class="social-wrap">
+					<a href="http://www.twitter.com/_hovelo" rel="nofollow"  class="social twitter"><span>@_hovelo</span></a>
+					<a href="http://www.facebook.com/hovelo" rel="nofollow"  class="social facebook"><span>Hovélo on facebook</span></a>
+					<a href="http://www.strava.com/clubs/hovelo" rel="nofollow" class="social strava"><span>Hovélo on Strava</span></a>
+				</p>
+			</div>
 		</article>
 
+		<article class="wrap plain hero">
+			<p class="meta">We're a friendly group of riders with one dedicated goal - to have&nbsp;fun. If you'd like to ride with us, we'd love to see you at the weekend.</p>
+			<p><strong>7:30am</strong> every <strong>Saturday</strong> and <strong>8:00am</strong> every <strong>Sunday</strong> outside the Goldstone Small Batch Coffee Company</p>
+		</article>
+
+		<div class="map" id="js-map"></div>
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+		<script type="text/javascript">
+		function initialize() {
+			var mapOptions = {
+				center: new google.maps.LatLng(50.833877, -0.171479),
+				zoom: 14,
+				scrollwheel: false,
+				draggable: false
+			};
+			var map = new google.maps.Map(document.getElementById("js-map"), mapOptions);
+			// To add the marker to the map, use the 'map' property
+			var marker = new google.maps.Marker({
+				position: mapOptions.center,
+				map: map,
+				title:"Small Batch"
+			});
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);
+		</script>
 
 		<article class="wrap">
-			<h2>Who is Hovélo for?</h2>
-			<p>Hovélo is open to everyone but you'll need to be comfortable riding at an average speed (including climbs...and there are some) of 14 - 16mph. </p>
+			<h2>What is Hovélo?</h2>
+			<p>Hovélo is a friendly and informal group of road cyclists. The age and ability varies massively and we're ultimately about enjoying a beautiful weekend ride.</p>
+			<p>Hovélo is open to everyone but you'll need to be comfortable riding at an average speed (including climbs...and there are some) of 14 - 16mph (22 - 25 kmph). </p>
 			<p>We'll be doing a couple of different paces for riders looking to get their speed and distances up and for experienced riders looking for a bigger challenge.</p>
 			<p>This isn't about tearing people's legs off or suffering - you can find that elsewhere. This is about getting used to riding in a group and having fun. There'll be chatting, jokes and discussions about cycling etiquette.</p>
 			<p>Shaved legs entirely optional for both sexes.</p>
-		</article>
-		<article class="wrap">
-			<h2>Hovélo rides</h2>
-			<p>There are 2 types of ride from Hovélo which generally alternate.</p>
-			<h3>Standard</h3>
-			<p>Between 20 and 40 miles - at a slightly slower pace, this is for everyone - especially those wanting to increase their regular distance and introduce people to the club. The pace of these rides along the flat will generally be between 15-18 mph.</p>
-			<p>Think focused bimble through some lovely scenery.</p>
-			<h3>Long</h3>
-			<p>More experienced riders can stretch their legs and their skills on these rides. They'll be anywhere between 45 - 70 miles (but generally between 50 and 60). The pace will be  faster than the standard rides but the chatting, jokes and banter won't differ much.</p>
-			<p>Great for riders training for sportives and upping their fitness levels.</p>
-			<p>Ride type and length will be detailed in the email newsletter or on our social media pages</p>
-		</article>
-		<article class="wrap">
-			<h2>What should I bring?</h2>
-			<ul>
-				<li>A road (or hybrid) bike</li>
-				<li>A helmet</li>
-				<li>Spare inner tube with pump and tyre levers</li>
-				<li>Water or sports drink</li>
-				<li>Food (such as energy bars)</li>
-				<li>A good sense of humour</li>
-			</ul>
-			<p>Food and 2 drinks bottles are very highly recommended on the <b>Long</b> rides.</p>
+
+			<h3>What should I bring?</h3>
+			<p>Although there will be people to help, you should be prepared for any eventuality. Make sure you have a <b>pump</b> and <b>spare inner tube</b> with you to fix that puncture, and plenty of <b>water</b> and <b>food</b> (such as energy bars) to keep you going.</p> Most importantly, bring along a <b>good sense of humour</b>!
 		</article>
 		<article class="wrap">
 			<h2>How do I Join?</h2>
-			<p>You simply turn up at one of the weekend rides.</p>
-			<p>Saturday morning is generally a Standard ride starting at <strong>7:30am</strong> outside the <a href="http://smallbatchcoffee.co.uk/locations/roastery-goldstone-villas-hove/" rel="nofollow">Goldstone Villas Small Batch Coffee Company</a></p>
-			<p>Sunday morning rides also start at Small Batch but have a start time of <strong>8am</strong>. Ride lengths vary, but will generally be a little longer.</p>
-			<p>Sign up to the newsletter to be kept up to date with that rides are coming up and an information about the club. You can also follow us on <a href="http://www.twitter.com/_hovelo" rel="nofollow">twitter</a> and <a href="http://www.facebook.com/hovelo" rel="nofollow">facebook</a>.</p>
-			<p class="social-wrap">
-				<a href="http://www.twitter.com/_hovelo" rel="nofollow"  class="social twitter">@_hovelo</a>
-				<a href="http://www.facebook.com/hovelo" rel="nofollow"  class="social facebook">Hovélo on facebook</a>
-				<a href="http://www.strava.com/clubs/hovelo" rel="nofollow" class="social strava">Hovélo on Strava</a>
-			</p>
+			<p>You simply turn up at one of the weekend rides!</p>
+			<p>Saturday morning rides start at <strong>7:30am</strong> outside the <a href="http://smallbatchcoffee.co.uk/locations/roastery-goldstone-villas-hove/" rel="nofollow">Goldstone Villas Small Batch Coffee Company</a></p>
+			<p>Sunday morning rides also start at Small Batch but have a start time of <strong>8am</strong>.</p>
 		</article>
 		<article class="wrap featured">
 			<form action="signup-process.php" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" novalidate>
@@ -128,20 +131,10 @@
 				<li>You get to explore Sussex and find new routes</li>
 				<li>You get to learn bike skills (including how to fix punctures quickly!)</li>
 				<li>You get to meet local cyclists</li>
-				<li>You get exclusive discounts at <a href="http://www.brightonbikemonkey.co.uk/" rel="nofollow">Brighton Bike Monkey</a> and more!</li>
+				<li>You get exclusive discounts at <a href="http://www.brightonbikemonkey.co.uk/" rel="nofollow">Brighton Bike Monkey</a>, <b>Small Batch Coffee Company</b>, <b>Prestige Cycles</b> and more!</li>
 			</ul>
 		</article>
 
-		<article class="wrap">
-			<h2 class="subtle">Previous E-mail Newsletters</h2>
-			<ul class="list">
-<?php foreach($data->mailchimp->campaigns as $c) : ?>
-				<li>
-					<a href="<?=$c->archive_url?>"><h3><?=$c->subject?> <time><?=$c->date_sent?></time></h3></a>
-				</li>
-<?php endforeach; ?>
-			</ul>
-		</article>
 		<script type="text/javascript" src="assets/js/scripts.js"></script>
 	</body>
 </html>
